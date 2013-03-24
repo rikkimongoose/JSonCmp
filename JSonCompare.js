6cab0eed3cfeafce72eeb7e43d98b2faff75cb8c
Rikki Mongoose <rikki@rikkimongoose.ru>
2013-03-24 15:09:11 +0400

    Options and Array compare
    + Add the Options param
    + Compare arrays
    + Compare arrays as sets

--- JSonCompare.js
+++ JSonCompare.js
@@ -30,6 +30,10 @@ function compareJSons(src1, src2) {
 			},
 			isObject : function(obj){
 				return (typeof obj == "object");
+			},
+			//ECMA Script standart check
+			isArray : function(obj) {
+				return Object.prototype.toString.call(obj);
 			}
 		},
 		FunctionAlg : {
@@ -75,7 +79,14 @@ function compareJSons(src1, src2) {
 			this._stackArray.pop();
 		}
 	};
-	var doComparation = function(sourceStr1, sourceStr2) {
+	var doComparation = function(sourceStr1, sourceStr2, options) {
+		var Options = {
+			arraysAsSets: false
+		};
+		if(options) {
+			if(CmpJSon.ObjectAlg.isExisting(options.arraysAsSets))
+				Options.arraysAsSets = options.arraysAsSets;
+		}
 		if(sourceStr1 == sourceStr2) {
 			return true;
 		}
@@ -89,6 +100,7 @@ function compareJSons(src1, src2) {
 				return false;
 			}
 		}
+		
 		var sourceObj1 = CmpJSon.ObjectAlg.prepareToCompare(sourceStr1),
 			lengthObj1 = CmpJSon.ObjectAlg.getObjectLength(sourceObj1),
 			sourceObj2 = CmpJSon.ObjectAlg.prepareToCompare(sourceStr2),
@@ -101,18 +113,45 @@ function compareJSons(src1, src2) {
 			if(CmpJSon.ObjectAlg.isObject(sourceObj2)) {
 				CmpStack.addObject(sourceObj1);
 				CmpStack.addObject(sourceObj2);
-				for(var propertyObject in sourceObj1) {
-					//debug output
-					//console.log(propertyObject + ":" + sourceObj1[propertyObject]);
-					//console.log(propertyObject + ":" + sourceObj2[propertyObject]);
-					var propertyObjectField1 = sourceObj1[propertyObject];
-					var propertyObjectField2 = sourceObj2[propertyObject];
-					if(CmpStack.objInCmpStack(propertyObjectField1) > -1 || CmpStack.objInCmpStack(propertyObjectField2) > -1) {
-						return (propertyObjectField1 === propertyObjectField1);
+				//Arrays support
+				if(CmpJSon.ObjectAlg.isArray(sourceObj1)) {
+					if(CmpJSon.ObjectAlg.isArray(sourceObj2)) {
+						if(Options.arraysAsSets) {
+							var iter1 = sourceObj1.length;
+							while(iter1--) {
+								var iter2 = sourceObj2.length,
+									isFoundInSet = false;
+								while(iter2--){
+									if(doComparation(sourceObj1[iter1], sourceObj2[iter2], Options)){
+										isFoundInSet = true;
+										break;
+									}
+								}
+								result = result && isFoundInSet;
+							}
+						} else {
+							var iter = sourceObj1.length;
+							while(iter--) {
+								doComparation(sourceObj1[iter], sourceObj2[iter], Options);
+							}
+						}
+					} else {
+						return false;
 					}
-					result = result && CmpJSon.ObjectAlg.isExisting(propertyObjectField2) && doComparation(propertyObjectField1, propertyObjectField2);
-					if(!result) {
-						break;
+				} else {
+					for(var propertyObject in sourceObj1) {
+						//debug output
+						//console.log(propertyObject + ":" + sourceObj1[propertyObject]);
+						//console.log(propertyObject + ":" + sourceObj2[propertyObject]);
+						var propertyObjectField1 = sourceObj1[propertyObject];
+						var propertyObjectField2 = sourceObj2[propertyObject];
+						if(CmpStack.objInCmpStack(propertyObjectField1) > -1 || CmpStack.objInCmpStack(propertyObjectField2) > -1) {
+							return (propertyObjectField1 === propertyObjectField1);
+						}
+						result = result && CmpJSon.ObjectAlg.isExisting(propertyObjectField2) && doComparation(propertyObjectField1, propertyObjectField2);
+						if(!result) {
+							break;
+						}
 					}
 				}
 				CmpStack.popLastObject(); // Remove sourceObj1
